7da5875826ba2e6e30a2558c5becac99e6adb51c
User-Takhirov <tohirovfayruz@gmail.com>
2024-12-18 23:36:22 +0500

    updated again

--- src/components/profile-card/profile-card.tsx
+++ src/components/profile-card/profile-card.tsx
@@ -4,12 +4,12 @@ import React from "react";
 
 export async function generateMetadata({ item }: { item: dataType }) {
   const metadata: Metadata = {
-    title: item.name,
+    title: "loremsdfsdjykfgtsdyjfgs",
     description: item.email,
   };
   return metadata;
 }
-export const ProfileCard = ({ item }: any) => {
+export const ProfileCard = ({ item }: { item: dataType }) => {
   return (
     <div className="flex flex-col shadow-2xl p-[20px] rounded-[10px]">
       <h1>{item.name}</h1>
